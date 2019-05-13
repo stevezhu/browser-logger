48ac3802c203fd0b9e36ee6c3261ac2632b560db
Steve Zhu <st.zhu1@gmail.com>
2019-05-13 21:08:06 +0900

    Convert tests into synchronous tests

--- index.spec.js
+++ index.spec.js
@@ -2,52 +2,50 @@ require('jest-extended')
 const { Logger } = require('./index')
 
 describe('Logger', () => {
+  beforeAll(() => {
+    console.info = function(...args) {
+      return args
+    }
+  })
+
   it('should create a method for each logging level', () => {
     const logger = new Logger()
-    expect(logger).toContainAllKeys(['debug', 'info', 'warn', 'error'])
+    expect(logger).toContainKeys(['debug', 'info', 'warn', 'error'])
   })
 
-  it('should output the correct arguments', done => {
-    console.info = function(...args) {
-      const [timestampFn] = args.splice(2, 1)
-      expect(timestampFn).toBeFunction()
-
-      expect(args).toEqual([
-        `[%c%s%c][%cinfo%c]`,
-        'color: gray; font-style: italic;',
-        '',
-        'color: #66bb6a; text-transform: uppercase;',
-        '',
-        'Some text',
-      ])
-
-      done()
-    }
-
+  it('should output the correct arguments', () => {
     const logger = new Logger()
-    logger.info('Some text')
+    const output = logger.info('Some text')
+
+    const [timestampFn] = output.splice(2, 1)
+    expect(timestampFn).toBeFunction()
+
+    expect(output).toEqual([
+      `[%c%s%c][%cinfo%c]`,
+      'color: gray; font-style: italic;',
+      '',
+      'color: #66bb6a; text-transform: uppercase;',
+      '',
+      'Some text',
+    ])
   })
 
-  it('should output the logger name if given', done => {
-    console.info = (...args) => {
-      const [timestampFn] = args.splice(2, 1)
-      expect(timestampFn).toBeFunction()
-
-      expect(args).toEqual([
-        `[%c%s%c][%cinfo%c][%ctest%c]`,
-        'color: gray; font-style: italic;',
-        '',
-        'color: #66bb6a; text-transform: uppercase;',
-        '',
-        'font-weight: bold',
-        '',
-        'Some text',
-      ])
-
-      done()
-    }
-
+  it('should output the logger name if given', () => {
     const logger = new Logger('test')
-    logger.info('Some text')
+    const output = logger.info('Some text')
+
+    const [timestampFn] = output.splice(2, 1)
+    expect(timestampFn).toBeFunction()
+
+    expect(output).toEqual([
+      `[%c%s%c][%cinfo%c][%ctest%c]`,
+      'color: gray; font-style: italic;',
+      '',
+      'color: #66bb6a; text-transform: uppercase;',
+      '',
+      'font-weight: bold',
+      '',
+      'Some text',
+    ])
   })
 })
